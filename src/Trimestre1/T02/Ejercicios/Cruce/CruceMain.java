bbbadb37f2c2828d0cc5677413e8e8afe7ba77f2
Pnavvaz <120363753+Pnavvaz@users.noreply.github.com>
2023-01-31 13:37:25 +0100

    k

--- src/Trimestre1/T02/Ejercicios/Cruce/CruceMain.java
+++ src/Trimestre1/T02/Ejercicios/Cruce/CruceMain.java
@@ -6,17 +6,17 @@ public class CruceMain {
     public static void main(String[] args) {
         Cruce cruce = new Cruce();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             Vehiculo vehiculo = new Vehiculo("Coche " + i, TiposVehiculo.COCHE, cruce,Colores.ANSI_YELLOW);
             vehiculo.setPriority(Thread.MAX_PRIORITY);
             vehiculo.start();
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             Vehiculo vehiculo = new Vehiculo("Camión " + i, TiposVehiculo.CAMION, cruce, Colores.ANSI_BLUE);
             vehiculo.setPriority(Thread.MAX_PRIORITY);
             vehiculo.start();
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             Vehiculo vehiculo = new Vehiculo("Bicicleta " + i, TiposVehiculo.BICICLETA, cruce, Colores.ANSI_GREEN);
             vehiculo.setPriority(Thread.MIN_PRIORITY);
             vehiculo.start();
